8832c8e11f8ec1e17512430f886cccec9778c35a
benste <benstmrs@gmail.com>
2024-09-11 10:11:36 +0200

    Add option to install poetry and dependencies when formatting codebase

--- scripts/format.sh
+++ scripts/format.sh
@@ -7,24 +7,29 @@ Help()
    echo "Syntax: format [-h]"
    echo "options:"
    echo "h      Print this Help."
-   echo
+   echo "p      setup dependencies through poetry before running the tests"
 }
 
-
-while getopts ":h" option; do
+setup_poetry=false
+while getopts ":hp" option; do
     case $option in
         h) # display Help
             Help
             exit;;
+        p) # setup poetry
+            setup_poetry=true
     esac
 done
 
-
-# Set-up poetry
-sh ./script/setup_poetry.sh -d
-
 # Make sure we exit on any errors
 set -e
+
+# Set-up poetry
+if [ $setup_poetry = true ]; then
+    echo "setting up Poetry"
+    sh ./scripts/setup_poetry.sh -d
+    echo $'\n\n\n'
+fi
 
 # Apply formatting
 echo "Running Black formatting..."
